c18c0aa88d4943cc61856e38884a0264d47cc795
Bowen Tan <btan2@andrew.cmu.edu>
2024-01-27 16:23:49 -0500

    updated.

--- project/run_machine_translation.py
+++ project/run_machine_translation.py
@@ -209,8 +209,8 @@ def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
     backend = backend=minitorch.TensorBackend(CudaKernelOps)
 
     config = {
-        'n_vocab'     : 2000,  # vocab_size
-        'n_embd'      : 128,   # n_embed
+        'n_vocab'     : 10000,  # vocab_size
+        'n_embd'      : 256,   # n_embed
         'n_head'      : 4,    # n_head
         'n_positions' : model_max_length,  # n_ctx == n_positions
         # 'n_layer'     : 4,    # n_layer
@@ -220,8 +220,8 @@ def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
     }
 
     model = DecoderLM(**config)
-    optimizer = minitorch.SGD(model.parameters(), lr=learning_rate)
-    # optimizer = minitorch.Adam(model.parameters(), lr=learning_rate)
+    # optimizer = minitorch.SGD(model.parameters(), lr=learning_rate)
+    optimizer = minitorch.Adam(model.parameters(), lr=learning_rate)
 
     dataset = {
         split: datasets.load_dataset(dataset_name, split=split)['translation']
@@ -230,7 +230,7 @@ def main(dataset_name='bbaaaa/iwslt14-de-en-preprocess',
     src_key, tgt_key = 'de', 'en'
 
     ### MAKE SMALLER
-    dataset['train'] = dataset['train'][:64]        # 160000
+    # dataset['train'] = dataset['train'][:20000]        # 160000
     dataset['validation'] = dataset['validation'][:64] # 7283
     dataset['test'] = dataset['test'][:5]             # 6750   
     ###
