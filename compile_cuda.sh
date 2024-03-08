496a660b7df3d134f472c9dac250bf812460959c
Bowen Tan <btan1@v002.ib.bridges2.psc.edu>
2024-03-07 21:51:44 -0500

    updated.

--- compile_cuda.sh
+++ compile_cuda.sh
@@ -1,3 +1,3 @@
 mkdir -p minitorch/cuda_kernels
 nvcc -o minitorch/cuda_kernels/combine.so --shared src/combine.cu -Xcompiler -fPIC
-nvcc -o minitorch/cuda_kernels/batched_matmul.so --shared src/batched_matmul_cublas.cu -lcublas
+# nvcc -o minitorch/cuda_kernels/batched_matmul.so --shared src/batched_matmul_cublas.cu -lcublas
